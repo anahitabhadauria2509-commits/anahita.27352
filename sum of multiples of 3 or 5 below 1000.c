2bcdb48b8ed2b4f00e8b1907c00fd6c3e49915b4
anahita singh <anahitabhadauria2509@gmail.com>
2025-08-26 11:14:04 +0530

    Add files via upload

---
+++ sum of multiples of 3 or 5 below 1000.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,sum=0;
+    for(i=1 ; i<100 ;i++)
+    {
+        if (i%3==0||i%5==0)
+        {
+           sum=sum+i;
+        }
+    }
+printf("sum=%d",sum);
+    return 0;
+}
